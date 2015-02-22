b15561c7ef3dab91fa2adaf6707d17b0a806ee62
LuckiiGoggy <luck-13@live.ca>
2015-02-21 20:32:17 -0800

    Finished Tutorial 5b Signed-off-by:LuckiiGoggy <luck-13@live.ca>

---
+++ application/views/admin_list.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<table cols="" border="0">
+  <tr>
+    <th>ID</th>
+    <th>Author</th>
+    <th>Mugshot</th>
+    <th>Their Saying</th>
+  </tr>
+  {quotes}
+  <tr>
+    <td>{id}</td>
+    <td>{who}</td>
+    <td>{mug}</td>
+    <td>{what}</td>
+  </tr>
+  {/quotes}
+</table> 
+<a href='/admin/add'>Add a new quotation</a>
---
+++ application/views/quote_edit.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<div class="row">
+  <div class="errors">{message}</div>
+  <form action="/admin/confirm" method="post">
+    {fid}
+    {fwho}
+    {fmug}
+    {fwhat}
+    {fsubmit}
+  </form>
+</div>
