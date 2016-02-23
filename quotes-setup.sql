00f212cdf880e9ee1312df5f042ba65fdaac2b19
arthur pankiewicz <arthurpankiewicz@gmail.com>
2016-02-22 21:42:17 -0800

    .sql dump

--- quotes-setup.sql
+++ quotes-setup.sql
@@ -1,48 +1,55 @@
--- phpMyAdmin SQL Dump
--- version 4.1.7
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.6.27, for osx10.8 (x86_64)
 --
--- Host: localhost
--- Generation Time: Feb 10, 2015 at 05:54 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: quotes
+-- ------------------------------------------------------
+-- Server version	5.6.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `contacts`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `quotes`
 --
 
-CREATE TABLE IF NOT EXISTS `quotes` (
+DROP TABLE IF EXISTS `quotes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quotes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `who` varchar(64) NOT NULL,
   `mug` varchar(64) NOT NULL,
   `what` text NOT NULL,
+  `vote_count` int(11) NOT NULL,
+  `vote_total` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `quotes` (`id`, `who`, `mug`, `what`) VALUES
-(1, 'Bob Monkhouse', 'bob-monkhouse-150x150.jpg', 'When I die, I want to go peacefully like my grandfather did–in his sleep. Not yelling and screaming like the passengers in his car.'),
-(2, 'Elayne Boosler', 'elayne-boosler-150x150.jpg', 'I have six locks on my door all in a row. When I go out, I lock every other one. I figure no matter how long somebody stands there picking the locks, they are always locking three.'),
-(3, 'Mark Russell', 'bob-monkhouse-150x150.jpg', 'The scientific theory I like best is that the rings of Saturn are composed entirely of lost airline luggage.'),
-(4, 'Anonymous', 'Anonymous-150x150.jpg', 'How do you get a sweet little 80-year-old lady to say the F word? Get another sweet little 80-year-old lady to yell “BINGO!”'),
-(5, 'Socrates', 'socrates-150x150.jpg', 'By all means, marry. If you get a good wife, you’ll become happy; if you get a bad one, you’ll become a philosopher.'),
-(6, 'Isaac Asimov', 'isaac-asimov-150x150.jpg', 'Those people who think they know everything are a great annoyance to those of us who do.');
+--
+-- Dumping data for table `quotes`
+--
 
+LOCK TABLES `quotes` WRITE;
+/*!40000 ALTER TABLE `quotes` DISABLE KEYS */;
+INSERT INTO `quotes` VALUES (1,'Bob Monkhouse','bob-monkhouse-150x150.jpg','When I die, I want to go peacefully like my grandfather did–in his sleep. Not yelling and screaming like the passengers in his car.',0,0),(2,'Elayne Boosler','elayne-boosler-150x150.jpg','I have six locks on my door all in a row. When I go out, I lock every other one. I figure no matter how long somebody stands there picking the locks, they are always locking three.',0,0),(3,'Mark Russell','bob-monkhouse-150x150.jpg','The scientific theory I like best is that the rings of Saturn are composed entirely of lost airline luggage.',0,0),(4,'Anonymous','Anonymous-150x150.jpg','How do you get a sweet little 80-year-old lady to say the F word? Get another sweet little 80-year-old lady to yell “BINGO!”',0,0),(5,'Socrates','socrates-150x150.jpg','By all means, marry. If you get a good wife, you’ll become happy; if you get a bad one, you’ll become a philosopher.',4,9),(6,'Isaac Asimov','isaac-asimov-150x150.jpg','Those people who think they know everything are a great annoyance to those of us who do.',5,17);
+/*!40000 ALTER TABLE `quotes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-02-22 21:40:59
