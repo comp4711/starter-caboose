eeec0de32493c6e77498086b01d2282fb6ff010b
Pablo Ramos <pablo.rms@gmail.com>
2016-02-28 17:16:12 -0800

    Lab 5   Fixed type on js

--- application/views/justone.php
+++ application/views/justone.php
@@ -3,7 +3,6 @@
     <div class="span8 offset1">
         <p class="lead">{what}</p><br/>
         <p class="text-right">{who}</p>
-        <p class="text-right">Number of votes: {vote_count}</p>
         <div class="hollywood" data-average="{average}" data-id="{id}">
         </div>
     </div>
